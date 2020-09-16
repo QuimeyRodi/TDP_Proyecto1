181b28302f92fad6a04a193853847ef607575c0f
QuimeyRodi <quimeyrodi@gmail.com>
2020-09-16 20:41:35 -0300

    Comienzo

---
+++ Prueba.java
@@ -0,0 +1,8 @@
+package TDP_Proyecto1;
+
+public class Prueba {
+	public static void main(String[] args) {
+		Persona p = new Persona("Quimey", 40858309);
+		System.out.println("nombre: " + p.getNombre() + " dni: " + p.getDni());
+	}
+}
